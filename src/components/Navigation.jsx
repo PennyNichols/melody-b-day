819339c979a2d6f43903fd2d1868a57e9c2020d6
pnichols789 <pnichols789@gmail.com>
2022-11-06 09:33:04 -0600

    -

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import {FaBirthdayCake} from 'react-icons/fa'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaBirthdayCake } from "react-icons/fa";
 
 const Navigation = () => {
-  return (
-    <Navbar  variant='dark' expand="md">
-      <Container fluid>
-        <Link to='/' style={{textDecoration: 'none', fontSize: '2rem'}} className='cake-wrapper' ><FaBirthdayCake className='cake'/></Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
-}
+	return (
+		<Navbar
+			className="sticky"
+			variant="dark"
+			expand="md"
+			style={{
+				backgroundColor: "rgba(255,255,255,.5)",
+				backdropFilter: "blur(5px)",
+			}}
+		>
+			<Container fluid>
+				<Link
+					to="/"
+					style={{ textDecoration: "none"}}
+					className="cake-wrapper"
+				>
+					<FaBirthdayCake style={{ fontSize: "3rem" }}className="cake" />
+				</Link>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto" style={{gap:'1.5rem'}}>
+						<Link style={{fontSize:'1.5rem',  textDecoration:'none'}} to="/">Home</Link>
+						<Link style={{fontSize:'1.5rem',  textDecoration:'none'}} to="/guest-list">RSVP</Link>
+						<Link style={{fontSize:'1.5rem',  textDecoration:'none'}}  to="/login">Login</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
 
-export default Navigation
+export default Navigation;
