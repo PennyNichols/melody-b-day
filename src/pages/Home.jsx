eb28eb8b83bc74d9eadd37a2e91d8f6e96c8d4df
pnichols789 <pnichols789@gmail.com>
2022-11-07 20:45:13 -0600

    -

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div>
-			<img src="https://github.com/PennyNichols/melody-b-day/blob/master/src/assets/title.png?raw=true" alt="Melody is turning 5!"  style={{width: '100vw'}}/>
+		<div style={{position: 'relative'}}>
+			<img src="https://github.com/PennyNichols/melody-b-day/blob/master/src/assets/title.png?raw=true" alt="Melody is turning 5!"   className='title'/>
+            <p className='details'>DEFY Birmingham November @ 10 AM
+
+            </p>
 		</div>
 	);
 };
