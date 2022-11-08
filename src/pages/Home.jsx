4168006b8c8b732cabc10355f4c40df07a4bbbde
pnichols789 <pnichols789@gmail.com>
2022-11-07 19:43:43 -0600

    -

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
 	return (
 		<div>
-			
+			<img src="https://github.com/PennyNichols/melody-b-day/blob/master/src/assets/title.png?raw=true" alt="Melody is turning 5!"  style={{width: '100vw'}}/>
 		</div>
 	);
 };
